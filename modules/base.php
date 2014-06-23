162e07c7f31d7a7c5474335ac44ffa16bd810cbe
horike <horike@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-23 04:23:01 +0000

    tagging version 1.4.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-simple-related-posts/tags/1.4.1@936864 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ modules/base.php
@@ -0,0 +1,85 @@
+<?php
+class Simple_Related_Posts_Base {
+	protected $option = '';
+
+	public function __construct() {
+		$this->option = get_option('sirp_options');
+		if ( $this->option['post_content'] == 1 )
+			add_filter( 'the_content', array($this, 'the_content') );
+	}
+	
+	public function the_content( $content ) {
+		if ( is_single() )
+			return $content.$this->get_reloated_posts();
+			
+		return $content;
+	}
+
+	public function get_reloated_posts( $num = '' ) {
+
+		$posts = $this->get_data( $num );
+		if ( !$posts )
+			return false;
+
+		$html = '<h2 class="simple-related-posts-title">' . apply_filters( 'sirp_title' , __( 'Related Posts', SIRP_DOMAIN ) ) . '</h2><ul class="simple-related-posts">';
+		foreach( $posts as $post ) {
+			$html .= '<li><a href="' . get_permalink( $post['ID'] ) . '" >';
+			$html .= apply_filters( 'sirp_before_post_title', '', $post['ID'] );
+			$html .= apply_filters( 'sirp_post_title', '<p class="title">' . get_the_title($post['ID']) . '</p>', $post['ID'] );
+			$html .= apply_filters( 'sirp_after_post_title', '', $post['ID'] );
+			$html .= '</a></li>';
+		}
+		$html .= '</ul>';
+
+		return $html;
+
+	}
+	public function get_data( $num = '' ) {
+		$related_posts = $this->get_data_post_meta($num, get_the_ID());
+		if ( !$related_posts )
+			$related_posts = $this->get_data_original($num, get_the_ID());
+			
+		return $related_posts;
+	}
+
+	public function get_data_post_meta( $num = '', $posts_id = null ) {
+		global $post;
+		
+		if ( !isset( $post_id ) )
+    		$post_id = $post->ID;
+    		
+    	if ( empty( $post_id ) )
+			return false;
+
+		$posts = get_post_meta( $post_id, 'simple_related_posts', true );
+		
+		if ( !$posts )
+			return false;
+		
+		$posts_ids = array();
+		foreach ( $posts as $id ) {
+			$posts_ids[]['ID'] = $id;
+		}
+		
+		$current_num = count($posts);
+		if ( $num == '' ) {
+			$option = get_option('sirp_options');
+			$display_num = intval($this->option['display_num']);
+		} else {
+			$display_num = intval($num);
+		}
+		
+		if ( $current_num > $display_num ) {
+			$num = $current_num - $display_num;
+			for ($i = 0; $i < $num; $i++) {
+				array_pop($posts_ids);
+			}
+			return $posts_ids;
+		} else {
+			return $posts_ids;
+		}	
+	}
+	
+	public function get_data_original( $num = '' ) {
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== WP Simple Related Posts ===
+Contributors: horike,megumithemes
+Tags:  related posts,related
+Requires at least: 3.8.1
+Tested up to: 3.9
+Stable tag: 1.4.1
+
+Related Posts plugin. It's flexible and fast and simple.
+
+== Description ==
+*Related posts based on categories, tags.
+*Can be replaced and sort of related posts at admin screen.
+*I can be added as an add-on another related posts algorithm.
+
+You can make addon to use [Addon Template](https://github.com/horike37/wp-simple-related-posts-addon-template/).
+
+= Translators =
+* Japanese(ja) - [Horike Takahiro](http://profiles.wordpress.org/horike)
+* Thai(th_TH) - [TG Knowledge](http://www.xn--12cg1cxchd0a2gzc1c5d5a.com)
+
+You can send your own language pack to me.
+
+Please contact to me.
+
+gmail:horike37@gmail.com
+
+= Contributors =
+* Horike Takahiro
+
+== Installation ==
+1. Upload `simple-related-posts` to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Screenshots ==
+1. post screen.
+2. option page.
+3. sigle page.
+
+== Changelog ==
+= 1.0 =
+* first release. 
+= 1.2.2 =
+* Add thumbnail image
+= 1.2.3 =
+* Add Related Posts title
+= 1.2.4 =
+* Related Posts addon with [Addon Template](https://github.com/horike37/wp-simple-related-posts-addon-template/)
+= 1.3 =
+* fixed bug on based tag and category
+* add pot file
+= 1.3.1 =
+* Thai support
+= 1.4 =
+* Edit CSS on Admin panel.
+* With a function to initialize the Related Posts.
+= 1.4.1 =
+* Bug fix
